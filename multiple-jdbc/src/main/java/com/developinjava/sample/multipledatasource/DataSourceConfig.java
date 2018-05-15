7ff93dfa55f002e3412159c80184d488eaeffb93
vardhan <vardhan@vardhan-Lenovo-G500>
2018-05-15 11:53:27 +0530

    multiple spring jdbc example

---
+++ multiple-jdbc/src/main/java/com/developinjava/sample/multipledatasource/DataSourceConfig.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.developinjava.sample.multipledatasource;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Bean
+	@Qualifier("customerDataSource")
+	@Primary
+	@ConfigurationProperties(prefix="customer.datasource")
+	DataSource customerDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@Qualifier("productDataSource")
+	@ConfigurationProperties(prefix="product.datasource")
+	DataSource productDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	@Qualifier("customerJdbcTemplate")
+	JdbcTemplate customerJdbcTemplate(@Qualifier("customerDataSource")DataSource customerDataSource) {
+		return new JdbcTemplate(customerDataSource);
+	}
+	
+	@Bean
+	@Qualifier("productJdbcTemplate")
+	JdbcTemplate productJdbcTemplate(@Qualifier("productDataSource")DataSource productDataSource) {
+		return new JdbcTemplate(productDataSource);
+	}
+}
---
+++ multiple-jdbc/src/test/java/com/developinjava/sample/MultipleJdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.developinjava.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultipleJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
